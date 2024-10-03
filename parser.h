eedb8eadeb64ac9f3f53a3f8c519416bb6ab1869
SleepMod <42214287+SleepMod@users.noreply.github.com>
2024-10-03 12:50:30 +0200

    Delete parser.h

--- parser.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-#include <vector>
-struct vec3d;
-
-void processLine(const std::string& line, std::vector<vec3d>& vertices);
-void parse(const char* path1, const char* path2);
