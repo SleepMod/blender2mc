57245e7105c1355ec22f2d424609494c79c73318
SleepMod <42214287+SleepMod@users.noreply.github.com>
2025-02-15 16:39:37 +0100

    Update parser.h

--- WaveObjParser/parser.h
+++ WaveObjParser/parser.h
@@ -4,4 +4,17 @@
 struct vec3d;
 
 void processLine(const std::string& line, std::vector<vec3d>& vertices);
-void parse(const char* path1, const char* path2);
+void convertObject(
+	const char* path1,
+	const char* path2,
+	float angleX,
+	float angleY,
+	float angleZ,
+	int x,
+	int y,
+	int z,
+	float amplitude,
+	std::string block,
+	std::string modifier
+);
+std::string setupString(std::string s1, std::string s2);
