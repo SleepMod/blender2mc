22123b16641d0ddb53b5480b48634bb7e01c22ff
SleepMod <42214287+SleepMod@users.noreply.github.com>
2025-02-15 16:39:22 +0100

    Update main.cpp

--- WaveObjParser/main.cpp
+++ WaveObjParser/main.cpp
@@ -1,3 +1,4 @@
+#include <Windows.h>
 #include <iostream>
 #include "parser.h"
 #include "colors.h"
@@ -11,9 +12,12 @@ void begin() {
     std::cin >> p1;
     std::cout << "[>] Path to save: ";
     std::cin >> p2;
-    std::cout << "[>] Connect vertexes? (1 for yes, 0 for no): ";
+    std::cout << "[>] Connect vertexes? (1 for yes, 0 for no): " << std::endl;
+
     int a = 0;
+    
     std::cin >> a;
+    
     if (a > 1 || a < 0) {
         withLine = false;
     }
@@ -21,18 +25,53 @@ void begin() {
         withLine = a == 1 ? true : false;
     }
 
-
     // ugly casting
     const char* path1 = p1.c_str();
     const char* path2 = p2.c_str();
 
-    parse(path1, path2);
+    std::string block = "minecraft:red_concrete";
+    std::string air = "minecraft:air";
+
+    // modifiers and constants
+    const float kAngleRotation = 15.0f;
+    float newAngle = 0.0f;
+    int ky = 0, kz = 0;
+
+    int x = 400, y = 36, z = -31;
+
+    // 408 36 -31
+    while (true) {
+
+        // hearts go here
+        convertObject(path1, path2, 0.0, newAngle, 0.0, x, y + ky, z, 2.0, block, "replace"); // block
+        Sleep(1500); // this sleep needs to be always here
+
+        // removers and updaters
+        convertObject(path1, path2, 0.0, newAngle, 0.0, x, y + ky, z, 2.0, air, "replace"); // air
+        Sleep(2000); // same for this one
+        
+        // sum the constant for the new angle 15 degrees for the yaw axis
+        newAngle += kAngleRotation;
+        ky++, kz++;
+        std::cout << "\nZ angle = " << newAngle << "\nDisplaying new obj.\n";
+        if (y >= 200) {
+            y = 200;
+        }
+    }
 }
 
 int main() {
-    rgbClamp col;
-    // 0 : 1
-    col = { 1, 0, 0 };
-    setupBlock(col);
-    return 0;
+    begin();
 }
+
+// DEBUG STUFF
+/*
+    Coordinates:
+    h1 = 251 83 -248
+    h2 = 215 89 -253
+    h3 = 304 91 -248
+*/
+
+/*
+    -100 20 915
+*/
